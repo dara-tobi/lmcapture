3f4481011767cc10299c509fc17026f32a65bab9
Dara Oladosu <daradosu@gmail.com>
2017-08-26 13:18:06 +0100

    get message reacted to

--- index.js
+++ index.js
@@ -2,8 +2,42 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var log = console.log;
+var request = require('request');
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function getMessage(item)
+{
+  var time = Math.round(item.ts);
+  var oldest = time - 1;
+  var latest = time + 1;
+
+  request.post({
+      url: 'https://slack.com/api/channels.history',
+      form: {
+        'token': 'xoxp-65743207921-65738846263-231467044272-948821a9fe039a287bde36f84c6e894a',
+        'channel': item.channel,
+        'oldest': oldest,
+        'latest': latest
+      }
+    },
+    function(err,httpResponse,body){ 
+      // log(body);
+      if (body.ok) {
+        for (var i = 0; i < body.messages.length; i++) {
+          if (body.messages[i].user == item.item_user && body.messages[i].ts == item.ts) {
+            var message = messages[i];
+          }
+        }
+        if (message) {
+          var text = message.text;
+          return "the message reacted to was: " + text;
+        }
+      }
+  });
+}
+
 
 app.get('/', function (req, res) {
    res.send('hello world');
@@ -17,8 +51,10 @@ app.get('/slack/reaction', function (req, res){
 app.post('/slack/reaction', function (req, res, next) {
   if (req.body.event.reaction) {
     if (req.body.event.reaction === 'grinning') {
-      log('grinning reaction added');
-      log(req.body);
+      // log('grinning reaction added');
+      var item = req.body.event.item;
+      log(getMessage(item));
+      // log(req.body);
     } else {
       log('non grinning reaction added');
     }
