18a11ea9002c8d7aa45d0a856d739a72d5c1c3d3
Dara Oladosu <daradosu@gmail.com>
2017-08-26 07:47:46 +0100

    use bodyparser

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', function (req, res) {
    res.send('hello world');
@@ -10,11 +13,8 @@ app.get('/slack/reaction', function (req, res){
   res.send('trying to get slack challenge');
 });
 
-app.post('/slack/reaction', function (req, res) {
-  console.log(req.params);
-  res.send(req.params.challenge);
-  res.send({name: 'dara', msg: 'welcome!!'});
-  res.send('POST request to homepage');
+app.post('/slack/reaction', function (req, res, next) {
+  res.send(req.body.challenge);
 });
 
 app.listen(process.env.PORT || 8000);
