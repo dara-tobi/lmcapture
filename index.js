285df10c4ca4f189d9d394ff64be2db9f7d4bf94
Dara Oladosu <daradosu@gmail.com>
2017-08-28 11:46:19 +0100

    add database

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ var app = express();
 var bodyParser = require('body-parser');
 var log = console.log;
 var request = require('request');
+var Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('', '', '', {
+  host: 'localhost',
+  dialect: 'sqlite',
+
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+
+  // SQLite only
+  storage: 'database.sqlite'
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
