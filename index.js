533419e96a8954594271b3d8d6bf00462b8d3f1a
Dara Oladosu <daradosu@gmail.com>
2017-09-09 17:59:26 +0100

    fix invalid scope

--- index.js
+++ index.js
@@ -218,7 +218,7 @@ app.get('/slack/auth', function (req, res) {
   log('receiving code');
   log('query', req.query.code);
   log('body', req.body);
-  res.redirect('https://slack.com/oauth/authorize?&client_id=65743207921.231877010403&scope=reactions:read,chat:write:bot,incoming-webhook,emoji:read,channels:history,im:history,im:read,im:write,bot,redirect_uri=https://lmedia.herokuapp.com/slack/access');
+  res.redirect('https://slack.com/oauth/authorize?&client_id=65743207921.231877010403&scope=reactions:read,chat:write:bot,incoming-webhook,emoji:read,channels:history,im:history,im:read,im:write,bot&redirect_uri=https://lmedia.herokuapp.com/slack/access');
   // request.post({
   //     url: 'https://slack.com/api/oauth.access',
   //     form: {
