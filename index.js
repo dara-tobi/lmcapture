370432d66215391fa3e93a63f33f6f5498a91ce3
Dara Oladosu <daradosu@gmail.com>
2017-09-11 05:49:52 +0100

    update help messages

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var log = console.log;
 var request = require('request');
 var db = require('./db');
 
-var couldNotRecommend = "Sorry, I couldn't find the resource you're trying to recommend. \n Add the `:resauce:` reaction on a post that has a link to get started.";
+var couldNotRecommend = "Sorry, I couldn't find the resource you're trying to recommend. \n Add the `:resauce:` reaction to a post that contains a link to get started.";
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -358,7 +358,7 @@ app.post('/slack/reaction', function (req, res, next) {
             } else if (req.body.event.text.toLowerCase() === 'no') {
               sendDirectMessage(req.body.event.channel, 'Okay, cancelling recommendation', bot_token);
             } else if (helpWords.indexOf(req.body.event.text.toLowerCase()) !== -1) {
-              sendDirectMessage(req.body.event.channel, "Add the `:resauce:` reaction to a post with a link to get started.", bot_token);
+              sendDirectMessage(req.body.event.channel, "Add the `:resauce:` reaction to a post that contains a link to get started.", bot_token);
             } else {
               // get last two messages, in order to confirm that the user is actually recommending something
               getTwoLatestMessages(req.body.event.channel, bot_token);
