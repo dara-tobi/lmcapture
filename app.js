ba311e9641625f176504fdaa711b928bb4512192
Bolaji Olajide <cooproton@gmail.com>
2019-09-19 22:32:42 +0100

    turn water to wine

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+module.exports = app;
---
+++ constants.js
@@ -0,0 +1,3 @@
+exports.getStartedMessage = "To get started, send me a link or add the `:resauce:` reaction to a post (in a public channel) that contains a link.";
+
+exports.couldNotRecommend = `Sorry, I couldn't find the resource you're trying to recommend. \n ${getStartedMessage}`;
