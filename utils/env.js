bd7ea62d1b71bf446d96ac0b23f565e3cbae9dc5
Dara <daradosu@gmail.com>
2019-10-27 08:00:37 +0100

    Revert "Turn Water into Wine with if..else statements"

--- utils/env.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  PORT: process.env.PORT,
-  RESAUCE_CLIENT_ID: process.env.resauce_client_id,
-  RESAUCE_CLIENT_SECRET: process.env.resauce_client_secret
-};
